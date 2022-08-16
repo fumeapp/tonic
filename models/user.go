108e6592c6d0cd4e66c4a5f6590dbf3732371d7d
kevin olson <acidjazz@gmail.com>
2022-08-16 14:24:23 -0500

    🚧 be sure to ignore all

--- models/user.go
+++ models/user.go
@@ -12,9 +12,8 @@ type User struct {
 	Avatar    string
 	Providers []Provider
 	Model
-	URL string `gorm:"-"`
+	URL string `gorm:"-:all"`
 }
-
 
 func (u *User) AfterFind(tx *gorm.DB) (err error) {
 	u.URL = fmt.Sprintf("/user/%d", u.ID)
