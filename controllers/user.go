2c85e252a746de28f7326d031e137dc15b589db2
kevin olson <acidjazz@gmail.com>
2022-08-16 14:04:26 -0500

    🚧 using AfterFind to set an accessor

--- controllers/user.go
+++ controllers/user.go
@@ -9,29 +9,25 @@ import (
 	"github.com/morkid/paginate"
 )
 
-func index(c *gin.Context) {
-	c.JSON(
-		http.StatusOK,
-		paginate.New().With(models.Db.Model(&models.User{}).Preload("Providers")).Request(c.Request).Response(&[]models.User{}),
-	)
-}
-
-func show(c *gin.Context) {
-	var user models.User
- 	models.Db.Model(&models.User{}).Preload("Providers").First(&user, c.Param("id"))
-	c.JSON(http.StatusOK, user)
-}
-
-func update(c *gin.Context) {
-	var user models.User
- 	models.Db.Model(&models.User{}).Preload("Providers").First(&user, c.Param("id"))
-	c.JSON(http.StatusOK, user)
-}
-
 func UserResource() route.ApiResourceStruct {
 	return route.ApiResourceStruct{
-		Index: index,
-		Show:  show,
-		Update: update,
+		Index: func(c *gin.Context) {
+			c.JSON(
+				http.StatusOK,
+				paginate.New().
+					With(models.Db.Model(&models.User{}).Preload("Providers")).
+					Request(c.Request).Response(&[]models.User{}),
+			)
+		},
+		Show: func(c *gin.Context) {
+			var user models.User
+			models.Db.Model(&models.User{}).Preload("Providers").First(&user, c.Param("id"))
+			c.JSON(http.StatusOK, user)
+		},
+		Update: func (c *gin.Context) { 
+			var user models.User
+			models.Db.Model(&models.User{}).Preload("Providers").First(&user, c.Param("id"))
+			c.JSON(http.StatusOK, user)
+		},
 	}
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,22 @@
 package models
 
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
 	Email     string `gorm:"unique"`
 	Name      string
 	Avatar    string
 	Providers []Provider
 	Model
+	URL string `gorm:"-"`
+}
+
+
+func (u *User) AfterFind(tx *gorm.DB) (err error) {
+	u.URL = fmt.Sprintf("/user/%d", u.ID)
+	return
 }
