123f7345b94722b170f4e3e54298c06e321e6b38
kevin olson <acidjazz@gmail.com>
2022-08-16 20:36:36 -0500

    🚧 more debug dat a- headers

--- routes/api.go
+++ routes/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/fumeapp/tonic/controllers"
 	"github.com/fumeapp/tonic/pkg/route"
 	"github.com/gin-gonic/gin"
+	"github.com/octoper/go-ray"
 )
 
 func Init(IsDev bool) *gin.Engine {
@@ -17,7 +18,11 @@ func Init(IsDev bool) *gin.Engine {
 	r.NoRoute(func(c *gin.Context) {
 		fmt.Println("Method: " + c.Request.Method)
 		fmt.Println("URL: " + c.Request.URL.String())
-		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+		for k, v := range c.Request.Header {
+			fmt.Println("Header: " + k + " " + v[0])
+		}
+		ray.Ray(c.Request)
+		c.JSON(404, gin.H{"code": "P4GE_NOT_FOUND", "message": "Page not found"})
 	})
 	return r
 }
