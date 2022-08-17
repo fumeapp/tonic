e46771dfe8b0ff228cc3f2f8f24ec21fe72c56b3
kevin olson <acidjazz@gmail.com>
2022-08-16 22:32:54 -0500

    🚧 back to default to false for APP_DEBUG

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -36,7 +36,7 @@ func IsDev () bool {
 	return env("APP_ENV", "dev") == "dev"
 }
 func IsDebug () bool {
-	return env("APP_DEBUG", "true") == "true"
+	return env("APP_DEBUG", "false") == "true"
 }
 
 
