85d2f6f9dd5e99496b38240025da5427d595f610
kevin olson <acidjazz@gmail.com>
2022-08-18 17:40:22 -0500

    ♻️ switch to our adapter

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"context"
-	"net/http"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-
+	fume "github.com/fumeapp/gin"
 	"github.com/fumeapp/tonic/models"
 	"github.com/fumeapp/tonic/pkg/setting"
 	"github.com/fumeapp/tonic/routes"
 )
-
-var ginLambda *ginadapter.GinLambdaV2
 
 func init() {
 
@@ -27,23 +19,6 @@ func init() {
 }
 
 func main() {
-
 	routes := routes.Init(setting.IsDev() || setting.IsDebug())
-
-	if (setting.IsDev()) {
-		server := &http.Server{
-			Addr:    ":8000",
-			Handler: routes,
-		}
-		server.ListenAndServe()
-	} else {
-		ginLambda = ginadapter.NewV2(routes)
-		lambda.Start(Handler)
-	}
-
-}
-
-func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
-	return ginLambda.ProxyWithContext(ctx, req)
+	fume.Start(routes, fume.Options{})
 }
