702822d97523bd6e1ca67012d96d2eb7b101bc99
kevin olson <acidjazz@gmail.com>
2022-08-19 01:47:49 -0500

    ✨ working 1st version of proper route model binding

--- route/route.go
+++ route/route.go
@@ -2,8 +2,12 @@ package route
 
 import (
 	"net/http"
+	"reflect"
+	"strconv"
 
+	. "github.com/fumeapp/tonic/database"
 	"github.com/gin-gonic/gin"
+	"github.com/octoper/go-ray"
 )
 
 type ApiResourceStruct struct {
@@ -13,6 +17,9 @@ type ApiResourceStruct struct {
 }
 
 var router *gin.Engine
+var modelType reflect.Type
+var apirs ApiResourceStruct
+var name string
 
 func Routes(route *gin.Engine) {
 	router = route
@@ -33,14 +40,44 @@ func RouteList(c *gin.Context) {
 			Handler: e.Handler,
 		})
 	}
-	c.JSON(
-		http.StatusOK,
-		routes,
-	)
+	c.JSON(http.StatusOK, routes)
 }
 
-func ApiResource(route *gin.Engine, resource string, ctls ApiResourceStruct) {
-	route.GET("/"+resource, ctls.Index)
-	route.GET("/"+resource+"/:id", ctls.Show)
-	route.PUT("/"+resource+"/:id", ctls.Update)
+func show (c *gin.Context) {
+	if checkNumeric(c) && retrieveModel(c) {
+		ray.Ray("and tell")
+		apirs.Show(c)
+	}
+}
+
+func ApiResource(route *gin.Engine, n string, model any, ctls ApiResourceStruct) {
+	apirs = ctls
+	modelType = reflect.TypeOf(model)
+	name = n
+	route.GET("/"+n, ctls.Index)
+	route.GET("/"+n+"/:id", show)
+	route.PUT("/"+n+"/:id", ctls.Update)
+}
+
+func checkNumeric(c *gin.Context) bool {
+	if _, err := strconv.Atoi(c.Param("id")); err != nil {
+		abortNotFound(c)
+		return false
+	}
+	return true
+}
+
+func retrieveModel(c *gin.Context) bool {
+	filled := reflect.New(modelType).Interface()
+	result := Db.Where("id = ?", c.Param("id")).First(filled)
+	if result.Error != nil {
+		abortNotFound(c)
+		return false
+	}
+	c.Set(name, filled)
+	return true
+}
+
+func abortNotFound(c *gin.Context) {
+	c.JSON(http.StatusNotFound, gin.H{"message": "Resource not found"})
 }
