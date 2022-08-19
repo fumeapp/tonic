639dcb6637b8a3254389527bbdcc77340b25c242
kevin olson <acidjazz@gmail.com>
2022-08-19 17:08:07 -0500

    🚧 publish new libraries

---
+++ cmd/cmd/migrate.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/fumeapp/tonic/database"
+	"github.com/octoper/go-ray"
+
+	"github.com/spf13/cobra"
+)
+
+// migrateCmd represents the migrate command
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Run migrations",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("migrate called")
+
+		ray.Ray(database.DSN())
+
+		// m, err := migrate.New("file://database/migrations", )
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// migrateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// migrateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/fumeapp/tonic/cmd/cmd"
+
+func main() {
+	cmd.Execute()
+}
