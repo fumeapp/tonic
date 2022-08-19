b2b04b7f8e21321e58f89f387f7f251070e38b12
kevin olson <acidjazz@gmail.com>
2022-08-18 20:30:23 -0500

    💚 format cleaned up

--- route/route.go
+++ route/route.go
@@ -7,23 +7,23 @@ import (
 )
 
 type ApiResourceStruct struct {
-	Index	func(c *gin.Context)
-	Show	func(c *gin.Context)
-		Update	func(c *gin.Context)
+	Index  func(c *gin.Context)
+	Show   func(c *gin.Context)
+	Update func(c *gin.Context)
 }
 
 func Routes(route *gin.Engine) {
 	route.GET("/", func(c *gin.Context) {
 		type RouteInfo struct {
-			Method string
-			Path   string
-			Handler  string
+			Method  string
+			Path    string
+			Handler string
 		}
 		routes := []RouteInfo{}
 		for _, e := range route.Routes() {
 			routes = append(routes, RouteInfo{
-				Method: e.Method,
-				Path:   e.Path,
+				Method:  e.Method,
+				Path:    e.Path,
 				Handler: e.Handler,
 			})
 		}
@@ -35,7 +35,7 @@ func Routes(route *gin.Engine) {
 }
 
 func ApiResource(route *gin.Engine, resource string, ctls ApiResourceStruct) {
-	route.GET("/" + resource, ctls.Index)
-	route.GET("/" + resource + "/:id", ctls.Show)
-	route.PUT("/" + resource + "/:id", ctls.Update)
+	route.GET("/"+resource, ctls.Index)
+	route.GET("/"+resource+"/:id", ctls.Show)
+	route.PUT("/"+resource+"/:id", ctls.Update)
 }
