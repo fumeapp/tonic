8aac13a7789c154c903d3c8329e3b0b5440eed0a
kevin olson <acidjazz@gmail.com>
2022-08-26 22:51:39 -0700

    ✨ aws connectivity - s3 upload helpers

--- aws/aws.go
+++ aws/aws.go
@@ -1,10 +1,17 @@
 package aws
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"io"
 	"log"
+	"net/http"
 
 	_ "github.com/aws/aws-sdk-go-v2"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -29,4 +36,58 @@ func Setup() {
 	Uploader = manager.NewUploader(S3)
 }
 
-func UNUSED(x ...any) {}
+// Uploads a file to S3 naming it after a hash of the file contents.
+// Accepts a public URL
+// returns the URL of the uploaded file and an error if there was one.
+func Upload(url string) (string, error) {
+
+	var extension string
+
+	response, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	bodyBytes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	contentType := http.DetectContentType(bodyBytes)
+
+	switch contentType {
+	case "image/jpeg":
+		extension = "jpg"
+	case "image/png":
+		extension = "png"
+	default:
+		return "", errors.New("unable to detect Content Type: " + contentType)
+	}
+
+	hasher := sha256.New()
+
+	if _, err := io.Copy(hasher, response.Body); err != nil {
+		return "", err
+	}
+
+	hash := hex.EncodeToString(hasher.Sum(nil))
+
+	uploader := manager.NewUploader(S3)
+
+	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket:      aws.String(setting.Aws.Bucket),
+		Key:         aws.String(hash + "." + extension),
+		Body:        io.NopCloser(bytes.NewBuffer(bodyBytes)),
+		ACL:         "public-read",
+		ContentType: aws.String(contentType),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.Location, nil
+
+}
